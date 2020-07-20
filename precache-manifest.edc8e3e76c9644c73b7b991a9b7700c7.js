fb197597055b5af2e87a2dbe3109a49472c1202a
yechubot <yechubot@yeeun-ui-MacBook-Air.local>
2020-07-20 12:56:38 +0900

    Updates

---
+++ precache-manifest.edc8e3e76c9644c73b7b991a9b7700c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498d2fa680d092fb377bdebef452e9d1",
+    "url": "/yechubot/movie_2020/index.html"
+  },
+  {
+    "revision": "9a60d4aae4c6ee94def8",
+    "url": "/yechubot/movie_2020/static/css/main.6151b12a.chunk.css"
+  },
+  {
+    "revision": "e2db976bfd17ee08d8a8",
+    "url": "/yechubot/movie_2020/static/js/2.9dbc7e27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yechubot/movie_2020/static/js/2.9dbc7e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a60d4aae4c6ee94def8",
+    "url": "/yechubot/movie_2020/static/js/main.7986d003.chunk.js"
+  },
+  {
+    "revision": "6df673445660b8a3168f",
+    "url": "/yechubot/movie_2020/static/js/runtime-main.5ecd8639.js"
+  }
+]);
