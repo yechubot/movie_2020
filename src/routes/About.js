17bc146fecec4f83818c7315ca3e86fc9af01b6a
yechubot <yechubot@yeeun-ui-MacBook-Air.local>
2020-07-20 12:54:27 +0900

    final code

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+function About() {
+    return (
+        <div className = "about__container">
+            <span> wow I cloned this app! </span>
+            <span>- yechubot, 7,19,2020</span>
+        </div>
+    );
+}
+
+export default About;
